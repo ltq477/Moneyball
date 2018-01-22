be520ce30d8454939ce3727d839cd1f097cbf114
Lawrence Quesada <ltquesada@gmail.com>
2018-01-22 01:07:09 -0800

    completed anaylysis

--- Moneyball.R
+++ Moneyball.R
@@ -7,6 +7,8 @@ library(tidyr)
 library(data.table)
 library(ggplot2)
 library(plotly)
+library(googleVis)
+
 
 #read dataframes from dataset
 df.batting <- read.csv('Batting.csv')
@@ -24,18 +26,20 @@ head(df.batting['X2B'])
 head(df.batting$X2B,5)
 
 #create new feature columns with formulas
+df.batting$X1B <- df.batting$H - df.batting$X2B - df.batting$X3B - df.batting$HR
 df.batting$BA <- df.batting$H / df.batting$AB
 df.batting$OBP <- (df.batting$H + df.batting$BB + df.batting$HBP) / (df.batting$AB + df.batting$BB + df.batting$HBP + df.batting$SF)
-df.batting$X1B <- df.batting$H - df.batting$X2B - df.batting$X3B - df.batting$HR
 df.batting$SLG <- (df.batting$X1B) +(2 * df.batting$X2B) + (3 * df.batting$X3B) + (4 * df.batting$HR) / df.batting$AB
+head(df.batting, 30)
 
 #view col names for references
 colnames(df.batting)
 colnames(df.salaries)
 
 #merge 2 dataframes by playerid, yeadid, teamid, lgid
-df <- merge(df.batting, df.salaries, by = c('yearID','teamID','lgID','playerID'),all=TRUE)
+df <- merge(df.batting, df.salaries, by = c('yearID','teamID','lgID','playerID'))
 summary(df)
+head(df, 30)
 
 #pull players on and after 1985
 df.1985 <- subset(df, yearID >=1985)
@@ -54,10 +58,22 @@ lost.players.2001[c('playerID','H','X2B','X3B','HR','OBP','SLG','BA','AB','yearI
 head(lost.players.2001)
 head(df)
 
-#pulled players who fit criteria
-for (players in df){
-    subset(df, salary <= sum(lost.players.2001$salary))
-}
+avail <- filter(df, yearID==2001) 
 
-subset(df, salary <= sum(lost.players.2001$salary))
+#OBP vs Salary
+plot.OBP.Sal <- avail[c('OBP','salary')] 
+plot(gvisScatterChart(plot.OBP.Sal))
 
+#AB vs Salary
+plot.AB.Sal <- avail[c('AB','salary')]
+plot(gvisScatterChart(plot.AB.Sal))
+
+#final 3 selected
+final <- avail %>% filter(salary<=5000000, OBP>0.34, AB>=600)  %>% arrange(AB, OBP, salary)
+final[1:3,]
+
+#Extra Notes 
+#1 view head(lost.players.2001) to compate and make decision
+#2 Combined salary  must not exceed $15,000,000. $15,000,000 / 3 = $5,000,000
+#3 lowest OBP is .34, so anything above that
+#4 anything above 600 was consistent so 600 was the chosen AB
